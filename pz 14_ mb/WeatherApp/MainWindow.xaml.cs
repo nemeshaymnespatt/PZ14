ef91926cf4a0c468d9c0aebbd88ba01f98c1bad6
nemeshaymnespatt <91611694+nemeshaymnespatt@users.noreply.github.com>
2024-03-11 21:15:04 +0500

    Add files via upload

---
+++ pz 14_ mb/WeatherApp/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows;
+
+namespace WeatherApp
+{
+    public partial class MainWindow : Window
+    {
+        private Random random;
+        private double currentTemperature;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            random = new Random();
+            currentTemperature = 0;
+
+            // Запускаем таймер, который будет обновлять температурные данные каждую секунду
+            System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(UpdateTemperature);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Start();
+        }
+
+        private void UpdateTemperature(object sender, EventArgs e)
+        {
+            // Генерируем новое значение температуры
+            currentTemperature = random.Next(-20, 20) ;
+
+            // Обновляем значение на экране
+            temperatureTextBlock.Text = currentTemperature.ToString("F1");
+
+            // Проверяем, если значение температуры находится в предусмотренном диапазоне (-5 до 5),
+            // то показываем уведомление пользователю
+            if (currentTemperature >= -5 && currentTemperature <= 5)
+            {
+                MessageBox.Show("Температура приближается к критической отметке!");
+            }
+        }
+    }
+}
---
+++ pz12/pz12/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Xml.Serialization;
+
+namespace pz12
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SerializeButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Создаем и инициализируем объект для сериализации
+            var obj = new MyObject
+            {
+                Name = "Мартемьянов Денис",
+                Age = 20,
+                Email = "sf252578@gmail.com"
+            };
+
+            // Создаем объект XmlSerializer
+            var serializer = new XmlSerializer(typeof(MyObject));
+
+            // Создаем файл для сохранения сериализованного объекта
+            var filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "serialized.xml");
+
+            // Сериализуем объект в XML и сохраняем его в файл
+            using (var writer = new StreamWriter(filePath))
+            {
+                serializer.Serialize(writer, obj);
+            }
+
+            MessageBox.Show("Объект успешно сериализован и сохранен в файле!");
+        }
+
+        private void DeserializeButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Создаем объект XmlSerializer
+            var serializer = new XmlSerializer(typeof(MyObject));
+
+            // Загружаем сериализованный объект из файла
+            var filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "serialized.xml");
+
+            using (var reader = new StreamReader(filePath))
+            {
+                // Десериализуем объект из XML
+                var deserializedObj = (MyObject)serializer.Deserialize(reader);
+
+                // Выводим информацию о десериализованном объекте
+                MessageBox.Show($"Имя: {deserializedObj.Name}\nВозраст: {deserializedObj.Age}\nEmail: {deserializedObj.Email}");
+            }
+        }
+    }
+
+    // Пример сериализуемого класса
+    public class MyObject
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+    }
+}
